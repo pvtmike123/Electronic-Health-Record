8e66363782cec46782f1f87e3126fd496b6f7d9b
Michael Fleming <flemin100@gmail.com>
2018-03-15 23:08:56 +0000

    Events query limited to 7

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,7 +4,7 @@ class EventsController < ApplicationController
   # GET /events
   # GET /events.json
   def index
-    @events = Event.all
+    @events = Event.all.order("updated_at DESC")
   end
 
   # GET /events/1
