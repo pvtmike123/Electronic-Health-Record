18391264342661fdbcc8ed115b4d1a29fec1e79a
Michael Fleming <flemin100@gmail.com>
2018-03-19 18:30:30 +0000

    Smoking status added to pie chart

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,7 @@ class HomeController < ApplicationController
   def index
     @patients = Patient.all.where(:user_id => current_user.id)
     @events = Event.all
+    @medical = Medical.all
     @conversations = current_user.mailbox.conversations
   end
 end
