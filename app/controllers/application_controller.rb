ae4b2f265b022a34a0ac4ad18a34c5c42f017dc5
Michael Fleming <flemin100@gmail.com>
2018-04-09 21:57:25 +0100

    Search refine edited, settings button removed from nav-bar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,4 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
-  def index
-    @conversations = current_user.mailbox.conversations
-  end
-
 end
